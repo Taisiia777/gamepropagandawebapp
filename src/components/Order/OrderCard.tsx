c85344c3a7d06b11a159e1675005deb22cc09ce0
Taisiia777 <sporyshev.savelii@gmail.com>
2024-10-27 17:35:21 +0300

    16

--- src/components/Order/OrderCard.tsx
+++ src/components/Order/OrderCard.tsx
@@ -1,28 +1,6 @@
-// import React from 'react';
-// import styles from './Orders.module.css';
-// import { OrderItem } from '../../types/Product.ts';
-//
-// interface OrderCardProps {
-//     order: OrderItem;
-// }
-//
-// export const OrderCard: React.FC<OrderCardProps> = ({ order }) => {
-//     return (
-//         <article className={styles.orderCard}>
-//             <span className={styles.orderType}>{order.type}</span>
-//             <h3 className={styles.orderTitle}>{order.title}</h3>
-//             <p className={styles.orderExpiry}>{order.expiryDate}</p>
-//             <p className={styles.orderPrice}>{order.price}</p>
-//             {order.canClaim && (
-//                 <button className={styles.claimButton}>
-//                     Забрать заказ
-//                 </button>
-//             )}
-//         </article>
-//     );
-// };
+
 import React from 'react';
-import styles from './Orders.module.css';
+import styles from './OrderCard.module.css';
 
 interface Item {
     id: number;
@@ -41,30 +19,47 @@ interface OrderCardProps {
 }
 
 export const OrderCard: React.FC<OrderCardProps> = ({ orderId, totalAmount, items, createdAt }) => {
+    console.log(items)
     return (
-        <article className={styles.orderCard}>
-            <h3 className={styles.orderTitle}>Заказ №{orderId}</h3>
-            <p className={styles.orderDate}>Дата заказа: {new Date(createdAt).toLocaleDateString()}</p>
-            <p className={styles.orderTotal}>Общая сумма: {totalAmount} руб.</p>
-
-            <div className={styles.orderItems}>
-                <h4>Товары в заказе:</h4>
-                {items.length > 0 ? (
-                    <div>
-                        {items.map((item) => (
-                            <div key={item.id} className={styles.orderItem}>
-                                <div>
-                                    <p className={styles.itemName}>{item.name}</p>
-                                    <p className={styles.itemQuantity}>Количество: {item.quantity}</p>
-                                    <p className={styles.itemPrice}>Цена: {item.newPrice} руб.</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                ) : (
-                    <p>Нет товаров в заказе</p>
-                )}
+        // <article className={styles.container}>
+        //     <h3 className={styles.orderTitle}>Заказ №{orderId}</h3>
+        //     <p className={styles.orderDate}>Дата заказа: {new Date(createdAt).toLocaleDateString()}</p>
+        //     <p className={styles.orderTotal}>Общая сумма: {totalAmount} руб.</p>
+        //
+        //     <div className={styles.orderItems}>
+        //         <h4>Товары в заказе:</h4>
+        //         {items.length > 0 ? (
+        //             <div>
+        //                 {items.map((item) => (
+        //                     <div key={item.id} className={styles.orderItem}>
+        //                         <div>
+        //                             <p className={styles.itemName}>{item.name}</p>
+        //                             <p className={styles.itemQuantity}>Количество: {item.quantity}</p>
+        //                             <p className={styles.itemPrice}>Цена: {item.newPrice} руб.</p>
+        //                         </div>
+        //                     </div>
+        //                 ))}
+        //             </div>
+        //         ) : (
+        //             <p>Нет товаров в заказе</p>
+        //         )}
+        //     </div>
+        // </article>
+        <section className={styles.container}>
+            <div className={styles.orderList}>
+                <span className={styles.dlcBadge}>DLC</span>
+                <h2 className={styles.orderTitle}>Заказ №{orderId}</h2>
+                <p className={styles.orderExpiry}>Дата окончания: {new Date(createdAt).toLocaleDateString()}</p>
+                <p className={styles.orderPrice}>{totalAmount} руб</p>
+                <button
+                    className={styles.orderButton}
+                    onClick={()=>{}}
+                    type="button"
+                >
+                    Забрать заказ
+                </button>
             </div>
-        </article>
+            <hr className={styles.divider}/>
+        </section>
     );
 };
